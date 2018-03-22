9bba44f8f41a1b5e158efa64a463ada7c9e0029f
Luzhijian <739788147@qq.com>
2018-03-23 02:28:48 +0800

    fix

---
+++ src/js/page4.js
@@ -0,0 +1,33 @@
+$(function(){
+  function carRun() {
+    $('#car').animate({
+      left: 90
+    },{
+      duration: 1500,
+      easing: 'linear',
+      complete: function() {
+        $('#welcome').slideDown(500, 'easeOutElastic')
+        setTimeout(function(){
+          $('#car').find('.car-sw').addClass('open');
+          // $('#audio').trigger('play');
+        }, 500)
+      }
+    });
+  }
+  carRun()
+
+  $('#control .btn').click(function(){
+    console.log('111');
+  })
+
+  $('body').on('click', '#control .btn', function() {
+    console.log('qweq')
+    var id = $(this).data('id');
+    console.log(id)
+    if (id === 1) {
+      alert('hello!')
+    } else {
+      location.href="./index.html"
+    }
+  })
+});
