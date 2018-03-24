cbd7d6cf7bbbb29c38a2c57a23f74e80c9b781fd
Luzhijian <739788147@qq.com>
2018-03-24 23:51:57 +0800

    up

--- dist/js/page4.js
+++
@@ -1,1 +0,0 @@
-"use strict";$(function(){function n(){$("#car").animate({left:90},{duration:1500,easing:"linear",complete:function(){$("#welcome").slideDown(500,"easeOutElastic"),setTimeout(function(){$("#car").find(".car-sw").addClass("open")},500)}})}n(),$("body").on("click","#control .btn",function(){var n=$(this).data("id");1===n?alert("hello!"):location.href="./index.html"})});
