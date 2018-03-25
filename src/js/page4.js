a18dfbf64a48906c814e98f3bfe01995aad2dcb2
Luzhijian <739788147@qq.com>
2018-03-26 01:11:24 +0800

    up

--- src/js/page4.js
+++ src/js/page4.js
@@ -1,27 +1,51 @@
 $(function(){
   function carRun() {
+    var bottleTop = $('#bottle').offset().top
+    var bottleLeft = $('#bottle').offset().left
+    var bool = new Parabola({
+      el: "#bottle",
+      offset: [60, 132],
+      curvature: 0.1,
+      duration: 4000,
+      callback: function() {
+        $('#bottle').addClass('swall');
+        $('#control').fadeIn();
+      },
+      stepCallback: function(x, y) {
+        $("<div>").appendTo(".top-part").css({
+          "position": "absolute",
+          "top": bottleTop + y,
+          "left": bottleLeft + x
+        });
+      }
+    });
     $('#car').animate({
-      left: 90
-    },{
+      left: 80
+    }, {
       duration: 1500,
       easing: 'linear',
       complete: function() {
         $('#welcome').slideDown(500, 'easeOutElastic')
-        setTimeout(function(){
+        setTimeout(function() {
           $('#car').find('.car-sw').addClass('open');
-          // $('#audio').trigger('play');
+          $('#bottle').css({
+            'opacity': 1
+          });
+          document.getElementById('audio').play()
+          bool.start();
         }, 500)
       }
     });
   }
-  carRun()
+  pet.wxShare('wxData', carRun, 'audio', 'bgm-2');
 
   $('body').on('click', '#control .btn', function() {
     var id = $(this).data('id');
+    $('#click-music').trigger('play');
     if (id === 1) {
-      alert('hello!')
+      // alert('hello!')
     } else {
-      location.href="./index.html"
+      location.href = "./index.html"
     }
   })
 });
